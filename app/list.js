502199b36f5fb9880817f7d8e7ec96bbb39a7612
Igor da Silva Dias <artscolha@gmail.com>
2025-10-16 17:29:18 -0300

    to-do-list-next-5 V1.0

---
+++ app/list.js
@@ -0,0 +1,45 @@
+"use client"
+
+import { useState } from 'react'
+
+export default function List() {
+    const [tasks, setTasks] = useState([])
+    const [input, setInput] = useState("")
+    const [error, setError] = useState("")
+
+    function add() {
+        if (input.trim() === "") {
+            setError("⚠️ Please, fill in this field ⚠️")
+
+            setTimeout(() => {
+                setError("")
+            }, 2000)
+
+            return
+        }
+        setTasks([...tasks, input])
+        setInput("")
+        setError("")
+    }
+    function clearAll() {
+        setTasks([])
+        setInput("")
+    }
+    return (
+        <div>
+            <h1>To-do list</h1>
+            {error && <p className='empty'>{error}</p>}
+            <input type="text" placeholder='Type a task...' value={input} onChange={(e) => setInput(e.target.value)} onKeyDown={(e) => {
+                if (e.key === 'Enter') add()
+            }} />
+            <button onClick={add}>Add</button>
+            <button onClick={clearAll}>Clear all</button>
+
+            <ul>
+                {tasks.map((tasks, i) => (
+                    <li key={i}>{tasks}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
